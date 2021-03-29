d27f054625078d5ac8f23a50432ef25a21bd3695
Erich Purpur <epurpur@gmail.com>
2021-03-29 13:21:03 -0400

    parsing 5 day weather forecast data and setting it in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,26 @@ var api_key = '333de4e909a5ffe9bfa46f0f89cad105'
 $('#searchBtn').click(function() {
     var cityName = $('input').val();
 
+    //remove HTML for today's weather card if any exists
+    $('#today-forecast').remove();
+
     //reset search text to empty
     $('input').val('');
     
     //draw HTML for today's weather card     
     makeTodaysHTML();
 
-    //make API request for today's weather
+    //make API call for today's weather
     getTodaysWeather(cityName);
 
     //add search value to list of Recent Searches
     addRecentSearch(cityName);
+
+    //remove HTML for 5 day forecast if any exists
+
+    //draw HTML for 5 day weather forecast
+
+    //make API call for 5 day weather
 });
 
 
@@ -144,6 +153,12 @@ function fillDailyWeather(data) {
 
 
 
+
+
+
+
+
+
 /**
  * FIVE DAY FORECAST
  */
@@ -161,8 +176,104 @@ function getFiveDayForecast(cityName) {
             } 
         })
         .then(function (data) {
-            console.log(data);
+            //set dates for 5 day forecast
+            fillForecastWeather(data);
         });
 }
 
 getFiveDayForecast(cityName);
+
+
+function fillForecastWeather(data) {
+    //uses 5 day weather forecast data returned from API call to set data for each card in 5 day forecast
+
+    //sets dates for 5 day forecast
+    setFiveDayForecastDates();
+
+    //parse each day's weather from API data. There are 5 items in fiveDayWeather 
+    var fiveDayWeather = parseFiveDayForecast(data);
+
+    //select necessary HTML elements
+    var weatherIcon = $('.five-day-weather')
+    var tempValue = $('.five-day-temp')
+    var humidityValue = $('.five-day-humidity')
+    
+    //use parsed data from fiveDayWeather to fill out HTML data
+    
+    //set weather icon  ####START HERE #####
+
+
+    //set temperature values
+    $.each(tempValue, function(index, value) {
+        value = $(value);                           //convert to jQuery
+        value.text(fiveDayWeather[index][1]);       //item[1] in fiveDayWeather array is temp value
+    });
+
+    //set humidity values
+    $.each(humidityValue, function(index, value) {
+        value = $(value);                           //convert to jQuery
+        value.text(fiveDayWeather[index][2]);       //item[2] in fiveDayWeather array is temp value
+    });
+
+}
+
+
+
+
+function parseFiveDayForecast(data) {
+    // console.log('parse', data.list);
+    
+    //Weather data returned in 3 hour increments for 5 days. Each 8 items in data.list is one day's weather
+    var allDaysRawData = [data.list.slice(0, 8), data.list.slice(8, 16), data.list.slice(16, 24), data.list.slice(24, 32), data.list.slice(32, 40)]
+
+    var allDaysFinalData = [];
+
+    $.each(allDaysRawData, function(index, item) {
+        var temps = 0;
+        var humidity = 0;
+        var weatherDescription;
+    
+        $.each(item, function(i, value) {
+            temps += value.main.temp;
+            humidity += value.main.humidity;
+
+            if (i == 0) {
+                weatherDescription = item[0].weather[0].main;
+            }
+        });
+        
+        temps = (temps / item.length).toFixed(2)            //get average temperature value, round to 2 decimal places
+        humidity = (humidity / item.length).toFixed(2)      //get average humidity value, round to 2 decimal places
+        
+        //finally, package weatherDescription, temps, humidity together and push to allDaysFinalData array
+        allDaysFinalData.push([weatherDescription, temps, humidity]);        
+    });
+    
+    return allDaysFinalData;
+}
+
+
+function setFiveDayForecastDates() {
+
+    var dateRange = []
+
+    var tomorrow = moment().add(1, 'days').format('MMMM D');
+    var twoDays = moment().add(2, 'days').format('MMMM D');
+    var threeDays = moment().add(3, 'days').format('MMMM D')
+    var fourDays = moment().add(4, 'days').format('MMMM D')
+    var fiveDays = moment().add(5, 'days').format('MMMM D')
+    
+    //add all dates to dateRange array
+    dateRange.push(tomorrow, twoDays, threeDays, fourDays, fiveDays);
+    
+    //set dates to .card-header tags in html
+    var cardHeaders = $('.card-header');
+    $.each(cardHeaders, function(index, value) {
+        value = $(value);                       //convert to jquery object
+        value.text(dateRange[index]);           //while looping through eaach value, use index of dateRange to set text of value
+    });
+}
+
+
+
+
